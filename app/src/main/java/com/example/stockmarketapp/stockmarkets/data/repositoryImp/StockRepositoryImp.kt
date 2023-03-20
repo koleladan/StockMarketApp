7e51630ee805a4ea5c64ba548ba70ce147381b3a
koleladan <isaiahodan@gmail.com>
2023-03-20 10:23:34 +0300

    more on data and domain packages

--- app/src/main/java/com/example/stockmarketapp/stockmarkets/data/repositoryImp/StockRepositoryImp.kt
+++ app/src/main/java/com/example/stockmarketapp/stockmarkets/data/repositoryImp/StockRepositoryImp.kt
@@ -1,8 +1,11 @@
 package com.example.stockmarketapp.stockmarkets.data.repositoryImp
 
 import com.example.stockmarketapp.core.util.Resource
+import com.example.stockmarketapp.stockmarkets.data.cvs.CSVParser
+import com.example.stockmarketapp.stockmarkets.data.cvs.CompanyListingsParser
 import com.example.stockmarketapp.stockmarkets.data.local.StockDatabase
 import com.example.stockmarketapp.stockmarkets.data.local.mapper.toCompanyListing
+import com.example.stockmarketapp.stockmarkets.data.local.mapper.toCompanyListingEntity
 import com.example.stockmarketapp.stockmarkets.data.remote.dto.StockApi
 import com.example.stockmarketapp.stockmarkets.domain.model.CompanyListing
 import com.example.stockmarketapp.stockmarkets.domain.repository.StockRepository
@@ -15,7 +18,8 @@ import javax.inject.Singleton
 @Singleton
 class StockRepositoryImp @Inject constructor(
     val api: StockApi,
-    val db: StockDatabase
+    val db: StockDatabase,
+    val companyListingsParser: CSVParser<CompanyListing>
 
 ): StockRepository {
 
@@ -42,17 +46,37 @@ class StockRepositoryImp @Inject constructor(
           }
           val remoteListings = try {
               val response = api.getListings()
-              response.byteStream()
+              companyListingsParser.parse(response.byteStream())
+
 
           } catch (e: IOException){
               e.printStackTrace()
               emit(Resource.Error( "An error occured"))
+              null
+              //parse null because we don't have remote data
 
           }catch (e: Exception){
               e.printStackTrace()
               emit(Resource.Error("Could not access Internet"))
+              null
+              //parse null because we don't have remote data
 
           }
+          //sticking to single source of truth values where data from api call is first loaded to the local database; cache
+          // then data on the UI is retrieved from the single source (local database: cache); data on the UI is not directly from the api call
+          remoteListings?.let {  listings ->
+              dao.clearCompanyListings()
+              dao.insertCompanyListings(
+                  listings.map { it.toCompanyListingEntity() }
+              )
+              emit(Resource.Success(
+                  data =dao
+                      .searchCompanyListing("")
+                      .map { it.toCompanyListing() }
+              ))
+              emit(Resource.Loading(false))
+          }
+
       }
     }
 }
